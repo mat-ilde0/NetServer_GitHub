0c694851e6c6cbe4516fbb51a3548779405ef280
Matilde Simonini <matilde.simonini@gmail.com>
2021-11-19 15:20:03 +0100

    implementate possibili conversazioni e metodo per vedere tutti i client connessi

--- NetServer/src/it/unibs/pajc/Protocol.java
+++ NetServer/src/it/unibs/pajc/Protocol.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /*
  * in questo modo nel metodo run si va ad implementare la logica di comunicazione
@@ -12,17 +13,23 @@ import java.net.Socket;
 public class Protocol implements Runnable{
 	
 	private Socket client;
+	private String clientName;
 	/*
 	 * non li inizializzo nel costruttore perché non ha senso costruire risorse se poi non le uso -> le costruisco nel metodo run perché è lì che le uso
 	 */
+	private static ArrayList<Protocol> clientList = new ArrayList<>();     //vengono memorizzati tutti i client connessi
+	/*
+	 * non ci ho messo Socket perchè andrei a memorizzare informazioni di basso livello e non potrei farci cose utili che mi servono.
+	 */
+
 	private BufferedReader in;
-	private PrintWriter out;
-	
+	private PrintWriter out;	
 	/*
 	 * Gli devo passare anche il BufferdReader e PrintWriter ? -> no: sono calcolati in funzione del socket del client.
 	 */
 	public Protocol(Socket client) {
 		this.client = client;
+		clientList.add(this);
 	}
 
 	@Override
@@ -49,7 +56,7 @@ public class Protocol implements Runnable{
 					out.printf("Ciao! Dimmi il tuo nome: ");
 					clientName = in.readLine();
 					
-				}while(clientName.length() < 3);    //se è sotto i tre caratteri non gho un nome valido
+				}while(clientName.length() < 3);    //se è sotto i tre caratteri non ho un nome valido
 				
 				out.printf("\nBenvenuto >>> %s <<<\n", clientName);
 				
@@ -61,15 +68,22 @@ public class Protocol implements Runnable{
 					 * stringa di risposta che si vuole mandare al client -> per farlo la mando nello stram in output.
 					 */
 					String command = request.toUpperCase();
-					
+									
 					if("!U".equals(command))
 						converToUpper = true;
 					else if("!L".equals(command))
 						converToUpper = false;
+					//comando per stampare tutti i client connessi al server
+					else if("@LIST".equals(command))
+						//viewClients();
+						listClient(this);
 					
 					//se incontro questa stringa termino la comunicazione con il server
 					if("@QUIT".equals(command))
 						break;
+					else 
+						sendToAll(this, request);
+
 					
 					String response = (converToUpper) ? 
 							request.toUpperCase(): request.toLowerCase();
@@ -85,6 +99,40 @@ public class Protocol implements Runnable{
 			ex.printStackTrace();
 		}
 		
+	}
+	
+	/*
+	 * metodo che trasmette un messaggio mandato da un client a tutti gli altri connessi al server
+	 */
+	protected void sendToAll(Protocol sender, String message) {
+		/*for (Protocol client : clientList) {    
+			client.sendMessage(sender, message);
+		}*/
+		
+		//Implementazione attraverso il processing Stream -> equivalente alla precedente.
+		clientList.forEach(c -> c.sendMessage(sender, message));
+	}
+	
+	/*
+	 * metodo che manda un messaggio al client che invoca il metodo : si scrive semplicemente sull'outputStream
+	 */
+	protected void sendMessage(Protocol sender, String message) {
+		this.out.printf("[%s] : %s", this.clientName, message);
+		this.out.flush();
+	}
+	
+	/*
+	 * metodo che permette, su richiesta di un client, per vedere tutti i client conessi 
+	 */
+	/*protected void viewClients() {
+		clientList.forEach(c -> this.out.printf("%s", c.clientName));
+	}*/  //creato io 
+	
+	//dal prof
+	protected void listClient(Protocol sender) {
+		clientList.forEach(c -> this.sendMessage(sender, String.format(" %s ", c.clientName)));
+		
+		this.sendMessage(sender, "\n\r");
 	}
 
 }
